182ca059e9460737153328a46226990977924157
aspiringde02 <aspiringde12@gmail.com>
2021-05-15 12:35:12 +0530

    Small change in jobs

--- jobs/app.py
+++ jobs/app.py
@@ -4,6 +4,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/templates')
+@app.route('/jobs')
 def jobs():
     return render_template('index.html')
